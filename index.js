a0c5c828f4d5ef8fac76633d4d4eb7805829d2c2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-02 17:54:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  const firstWin = record.find(entry => entry.result == "W");
+  // if (firstWin == undefined){
+  //   return undefined;
+  // }else{
+  //   return firstWin;
+  // }
+  return (firstWin == undefined) ? undefined : firstWin.year;
 }
